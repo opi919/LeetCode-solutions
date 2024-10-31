1f143807c3c250bc52f4ef41850b1365e7b71ab5
Momen Khandoker <47396047+opi919@users.noreply.github.com>
2024-10-31 23:02:57 +0600

    Time: 12 ms (32.34%), Space: 10.8 MB (85.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,20 +8,17 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head)
-    { 
-        if(!head) return false;
-
-        ListNode *slow = head;
-        ListNode *fast = head;
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
         while(fast && fast->next){
-            fast = fast->next->next;
             slow = slow->next;
-
+            fast = fast->next->next;
             if(slow == fast) return true;
+
         }
-    
+
         return false;
     }
 };
